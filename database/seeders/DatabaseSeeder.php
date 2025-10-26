3938f4dbcafdfdbc43ad0679449dbe3ccc68ffc8
Antsac <antsafiderana333@gmail.com>
2025-10-26 18:25:36 +0300

    Remove : quotation and quotation lines

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,6 @@ namespace Database\Seeders;
 use App\Models\Customer;
 use App\Models\Option;
 use App\Models\Project;
-use App\Models\Quotation;
-use App\Models\QuotationLine;
 use App\Models\SolarPanel;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -27,39 +25,20 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@powerio.com',
         ]);
 
-        // Create solar panels
         SolarPanel::factory(5)->create();
 
-        // Create options (10 predefined options)
         Option::factory(10)->create();
 
-        // Create customers (some will have multiple projects)
         $customers = Customer::factory(15)->create();
 
-        // Create projects - some customers will have multiple projects
         $projects = collect();
 
-        // Give some customers multiple projects
         $customers->each(function ($customer) use (&$projects) {
             $projectCount = fake()->numberBetween(1, 3); // Each customer gets 1-3 projects
             $customerProjects = Project::factory($projectCount)->create([
                 'customer_id' => $customer->id,
             ]);
             $projects = $projects->merge($customerProjects);
-        });
-
-        // Create quotations for some projects
-        $projects->random(min(10, $projects->count()))->each(function ($project) {
-            Quotation::factory(rand(1, 2))->create([
-                'project_id' => $project->id,
-            ]);
-        });
-
-        // Create quotation lines
-        Quotation::all()->each(function ($quotation) {
-            QuotationLine::factory(rand(3, 8))->create([
-                'quotation_id' => $quotation->id,
-            ]);
         });
     }
 }
