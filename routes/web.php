3999dcdb321ba4102c23a8a1c33b5264a2b6c178
Antsac <antsafiderana333@gmail.com>
2025-10-27 07:32:14 +0300

    Update : redirect admin

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\QuotationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
 
 Route::prefix('quotations')->name('quotations.')->group(function () {
     Route::get('{quotation}/pdf/download', [QuotationController::class, 'downloadPDF'])->name('pdf.download');
